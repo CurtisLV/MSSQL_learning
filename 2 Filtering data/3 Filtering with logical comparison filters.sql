02957fd82530be2cadb0500cb4d59bfda3a84148
CurtisLV <curtislv@gmail.com>
2022-08-07 17:52:38 +0300

    17:52

--- 2 Filtering data/3 Filtering with logical comparison filters.sql
+++ 2 Filtering data/3 Filtering with logical comparison filters.sql
@@ -1,1 +1,27 @@
 use AdventureWorks2016
+go
+
+--select *
+--from Person.Person;
+
+/*
+select FirstName, LastName, PersonType
+from Person.Person
+where PersonType = 'SC' OR PersonType = 'VC';
+go
+*/
+
+select FirstName, LastName, PersonType, EmailPromotion
+from Person.Person
+where PersonType = 'SC' and EmailPromotion = 2;
+go
+
+select FirstName, LastName
+from Person.Person
+where FirstName IN('Fred', 'Mary', 'George');
+go
+
+select *
+from HumanResources.Employee
+where SickLeaveHours between 40 and 99;
+go
