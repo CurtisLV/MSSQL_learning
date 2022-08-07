e6dc8035e94d62bcb09c23d8b318ed138f878585
CurtisLV <curtislv@gmail.com>
2022-08-07 19:42:03 +0300

    19:41

--- 5 Aggregate functions/1 Count, AVG, MAX, MIN, SUM.sql
+++ 5 Aggregate functions/1 Count, AVG, MAX, MIN, SUM.sql
@@ -1,10 +1,29 @@
--- Count() - returns total number of rows
-use AdventureWorks2016
- 
-select COUNT(*) as "Total employees"
-from HumanResources.Employee;
+USE AdventureWorks2016
 
-select COUNT(distinct JobTitle) as [Number of titles]
-from HumanResources.Employee;
+-- Count() - returns total number of rows
+SELECT COUNT(*) AS "Total employees"
+FROM HumanResources.Employee;
+
+SELECT COUNT(DISTINCT JobTitle) AS [Number of titles]
+FROM HumanResources.Employee;
 
 -- AVG() - returns average of a NUMERIC column
+SELECT AVG(SickLeaveHours) AS [Average sick hours]
+FROM HumanResources.Employee;
+
+SELECT AVG(VacationHours) AS [Avg holiday hours]
+FROM HumanResources.Employee;
+
+-- MAX() - returns largest value from a specified column
+SELECT MAX(SickLeaveHours) AS [Max sick hours]
+FROM HumanResources.Employee;
+
+-- MIN() - returns smallest value from a specified volume, ignores null values
+SELECT MIN(SickLeaveHours) AS [Min sick hours]
+FROM HumanResources.Employee;
+
+-- SUM() - returns total value of a numeric column
+SELECT SUM(SickLeaveHours) AS [Sum of sick hours]
+FROM HumanResources.Employee;
+
+ 
